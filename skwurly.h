8a46efb1b3154201a04a595625a286e55f900360
Jason Mooberry <jasonmoo@me.com>
2012-08-25 19:47:16 -0400

    Fix for gcc warning

--- skwurly.h
+++ skwurly.h
@@ -1,10 +1,10 @@
 #ifndef __SKWURLY_H_
 #define __SKWURLY_H_
 
-	typedef struct param {
+	typedef struct _param {
 		const char* start;
 		int length;
-	};
+	} param;
 
 	char* url_sort(const char *url);
 
